ea2f51a8059ee84ccf6874f0eb41361f36c8bc56
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-03-14 10:40:25 -0600

    [AGREGA] estructura y archivos. No se que mas decir

---
+++ mvvm/mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Jadzia Gallegos on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mvvm/mvvmTests/mvvmTests.swift
@@ -0,0 +1,16 @@
+//
+//  mvvmTests.swift
+//  mvvmTests
+//
+//  Created by Jadzia Gallegos on 14/03/25.
+//
+
+import Testing
+
+struct mvvmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
