96cd3574586860c8c1973d4b6d3fde3b709d4737
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-03-19 10:45:50 -0600

    [AGREGA] nuevas cositas y yun poquito de edicion magica

---
+++ mvvm2/mvvm2/vistas_modelos/estados_de_la_aplicacion.swift
@@ -0,0 +1,12 @@
+//
+//  estados_de_la_aplicacion.swift
+//  mvvm2
+//
+//  Created by Jadzia Gallegos on 19/03/25.
+//
+
+enum EstadosDeLaAplicacion{
+    case mostrando_series
+    case agregando_series
+    case penzando_en_la_inmortalidad_del_cangrejo
+}
--- mvvm2/mvvm2/vistas_modelos/vista_modelo_basico.swift
+++ mvvm2/mvvm2/vistas_modelos/vista_modelo_basico.swift
@@ -8,10 +8,22 @@ import Foundation
 
 @Observable
 class VistaModeloBasico{
+    var estado_actual_de_la_aplicacion: EstadosDeLaAplicacion = .mostrando_series
+    
     var series_registradas: Array<InformacionSerie> = []
     
-    func agregar_serie() -> Void{
-        print("HOla mundo desde el controlador")
-        series_registradas.append(InformacionSerie(nombre: "prueba", tipo: "prueba", fecha_estreno: 2017, sinopsis: "Aqui va una sinopsis muy larga", caratula: "imagen_fake_1"))
+    func agregar_serie(serie: InformacionSerie? = nil) -> Bool{
+        if let serie_nueva = serie {
+            if serie_nueva.nombre == ""{
+                return false
+            }
+            
+            series_registradas.append(serie_nueva)
+        }
+        else {
+            series_registradas.append(InformacionSerie(nombre: "prueba", tipo: "prueba", fecha_estreno: 2017, sinopsis: "Aqui va una sinopsis muy larga", caratula: "imagen_fake_1"))
+        }
+        
+        return true
     }
 }
